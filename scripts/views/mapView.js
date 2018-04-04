ef79f7163e9be0f5d310cf0439214ed255869f36
Joanna Coll <johannam999@gmail.com>
2018-04-04 15:09:27 -0700

    legend

--- scripts/views/mapView.js
+++ scripts/views/mapView.js
@@ -1,6 +1,30 @@
 'use strict';
 
+var icons = {
+  play: {
+    name: 'Play Areas',
+    icon: 'img/childIcon.png'
+  },
+  dogs: {
+    name: 'Dog Off-Leash Areas',
+    icon: 'img/dogIcon.png'
+  },
+  water: {
+    name: 'Waterfront Areas',
+    icon: 'img/waterIcon.png'
+  }
+};
 
+var legend = document.getElementById('legend');
+
+for (var key in icons) {
+var type = icons[key];
+var name = type.name;
+var icon = type.icon;
+var div = document.createElement('div');
+div.innerHTML = '<img src="' + icon + '"> ' + name;
+legend.appendChild(div);
+}
 
 var autocomplete;
 
