7d74ff6535f903185947967c65908fec40d11761
Joanna Coll <johannam999@gmail.com>
2018-04-05 09:38:36 -0700

    more push

--- scripts/views/aboutUs.js
+++ scripts/views/aboutUs.js
@@ -3,7 +3,7 @@ var app = app || {};
 const ENV = {};
 
 ENV.isProduction = window.location.protocol === 'https:';
-ENV.productionApiUrl = 'https://play-n-rest.herokuapp.com/';
+ENV.productionApiUrl = 'https://play-n-rest.herokuapp.com';
 ENV.developmentApiUrl = 'http://localhost:3000';
 ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
 //Array of Objects containing our About Us information.
