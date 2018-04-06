dc38f2f86960d523fd73c82b21fc6ac643f9daf7
Joanna Coll <johannam999@gmail.com>
2018-04-06 11:25:29 -0700

    linking

--- scripts/views/aboutUs.js
+++ scripts/views/aboutUs.js
@@ -2,10 +2,10 @@
 var app = app || {};
 const ENV = {};
 
-ENV.isProduction = window.location.protocol === 'https:';
-ENV.productionApiUrl = 'https://play-n-rest.herokuapp.com';
-ENV.developmentApiUrl = 'http://localhost:3000';
-ENV.apiUrl = ENV.isProduction ? ENV.productionApiUrl : ENV.developmentApiUrl;
+// ENV.isProduction = window.location.protocol === 'https:';
+// ENV.productionApiUrl = 'https://play-n-rest.herokuapp.com';
+// ENV.developmentApiUrl = 'http://localhost:3000';
+ENV.apiUrl = 'https://play-n-rest.herokuapp.com';
 //Array of Objects containing our About Us information.
 (function(module) {
 
